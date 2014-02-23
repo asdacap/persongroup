f8c0d65677b14f4ef8ced35dab133c4cae778245
Muhd Amirul Ashraf <asdacap@gmail.com>
2014-02-23 19:40:29 +0800

    Added the people's functionality and bootstrap.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,25 +2,45 @@ class PeopleController < ApplicationController
     
     
     def index
+        @people=Person.all
     end
 
     def create
+        @person=Person.new(person_params)
+        if @person.save
+            redirect_to person_path(@person.id) 
+        else
+            render :new
+        end
     end
 
     def new
+        @person=Person.new
     end
 
     def edit
+        @person=Person.find(params[:id])
     end
 
     def show
+        @person=Person.find(params[:id])
     end
 
     def update
+        @person=Person.find(params[:id])
+        @person.update(person_params) and redirect_to person_path(@person) and return
+        render :edit
     end
 
     def destroy
+        @person=Person.find(params[:id])
+        @person.destroy
+        redirect_to people_path
     end
 
+    private
+        def person_params
+            params.require(:person).permit(:name,:address,:phone)
+        end
 
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,7 @@
 class Person < ActiveRecord::Base
+    
+    validates :name,:phone,:address, presence: true
+
+    validates :phone, format: { with: /\A\+?[0-9\-]{8,13}\z/, message: "Please enter a valid phone number" }
+
 end
